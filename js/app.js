b1c6b0de6b01a88720a2a89895051fdda2689a5d
guiiaquinto <gui_iaquinto@outlook.com>
2023-03-10 23:03:26 -0300

    Add JS

---
+++ js/app.js
@@ -0,0 +1,12 @@
+
+function rest(){
+var formsFilme = document.querySelector('#nomeFilme').value;
+var formsDesc = document.querySelector('#desc').value;
+var formsUrl = document.querySelector('#url').value;
+
+document.querySelector('#resultado').innerHTML = formsFilme;
+document.querySelector('#resultado').innerHTML = formsDesc;
+document.querySelector('#resultado').innerHTML = formsUrl;
+
+}
+
